e22fa5428d08bf234f4a01225ecbc023280639eb
malans <marcusegues@gmail.com>
2016-06-22 21:46:07 +0200

    Playable game.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,5 +1,5 @@
 require_relative 'board'
-require 'byebug'
+require 'yaml'
 
 class Minesweeper
   def initialize
@@ -12,25 +12,38 @@ class Minesweeper
       @board.render
       puts "Enter a position [row, col]"
       position = gets.chomp.split(", ").map(&:to_i)
-      puts "Reveal, flag, or unflag? [r/f/u]"
+      puts "Reveal, flag, unflag, or save? [r/f/u/s]"
       option = gets.chomp
       if option == "r"
         won = @board.reveal(position)
         break unless won
       elsif option == "f"
         @board[position].flag
-      else
+      elsif option == "u"
         @board[position].unflag
+      else
+        save
+        won = "saved"
+        break
       end
       # break unless won
     end
 
-    if won
+    if won == "saved"
+      puts "Game saved!"
+    elsif won
       puts "You win!"
     else
       puts "You lose!"
     end
 
+  end
+
+  def save
+    puts "Enter filename to save at:"
+    filename = gets.chomp
+
+    File.write(filename, self.to_yaml)
   end
 
   def game_over?
@@ -39,5 +52,12 @@ class Minesweeper
 end
 
 
-board = Minesweeper.new
-board.play
+if $PROGRAM_NAME == __FILE__
+
+  case ARGV.count
+  when 0
+    Minesweeper.new.play
+  when 1
+    YAML.load_file(ARGV.shift).play
+  end
+end
